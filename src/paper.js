ce5103c4fe299405ba03d584440b9d7dcd324ff8
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-22 18:21:54 +0000

    added comments to paper

--- src/paper.js
+++ src/paper.js
@@ -5,6 +5,8 @@ export default function Paper() {
         <div className="toDoPage">
             <h1 className="title">To Do List</h1>
             <input className="inputBox" type='text'></input>
+            {/* this div below will be a child that is imported that is made up of other children that all have 
+            onclicks and buttons as well as a label that is the prop of the input value */}
             <div className="tasks">
                 <ul>
                     <li>create my app</li>
@@ -15,6 +17,7 @@ export default function Paper() {
             <button className="all">All</button>
             <button className="active">Active</button>
             <button className="done">Done</button>
+            {/* this div will update based on the current active count */}
             <div className="counter">displays left to do</div>
             <button className="clearAll">Clear All</button>
         </div>
