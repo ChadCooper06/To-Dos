8b3154977a09d255adcbda8fea31df963a026497
ChadCooper06 <chad.cooper@student.awesomeinc.org>
2022-03-23 11:48:04 +0000

    added window.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Paper from "./paper";
 import "./App.css";
+import WindowSize from "./window";
 
 function App() {
+
+  const size = WindowSize();
+
   return (
     <div className="App">
+      {size.width}px / {size.height}px
       <Paper />
     </div>
   );
